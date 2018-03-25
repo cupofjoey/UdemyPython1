aac9b93519660cde1621efa1c406f69858dbd0e4
Joe Hill <joe@jkh3.com>
2018-03-24 22:58:11 -0400

    Added the .format method to my string. Pretty nifty. Commented out the old method in input.py

--- input.py
+++ input.py
@@ -1,3 +1,5 @@
 age = input("Enter your age: ")
 #need to remember to put in a control on what the user can input. Like only accept ints rather than strings.
-print("You have lived for " + str(int(age) * 365 * 24 * 60 * 60) + ' seconds. Congratulations.')
+#that's where the input method comes in
+#print("You have lived for " + str(int(age) * 365 * 24 * 60 * 60) + " seconds. Congratulations.")
+print('You have lived for {} seconds. Congratulations. This corresponds to {} years.'.format(int(age) * 365 * 24 * 60 * 60, age))

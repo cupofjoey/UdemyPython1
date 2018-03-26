496771f5b912cf59ec32466aebc9a387f28c8c2c
Joe Hill <joe@jkh3.com>
2018-03-25 22:19:36 -0400

    Added lists.py, which covers a little bit of lists/arrays in Python. Yippee

---
+++ lists.py
@@ -0,0 +1,9 @@
+numbers = [0,1,2,3,4,5,6,7,8,9]
+ #Not much to do here. Gotta be honest...just gonna...look around a bit. 
+numbers[len(numbers) -1]
+#output is '9'
+numbers[3]
+#output is '3'
+#...ok...so....that's that. Zzzzzzzz. I'm sure it will get better I'm just in syntax hell right now. 
+#Congrats if you're reading this. 
+

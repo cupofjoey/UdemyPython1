19702fd06b497f9f9d260f1c9c85dc594fe8e8d3
Joe Hill <joe@jkh3.com>
2018-03-25 22:30:34 -0400

    Added some snarky comments, and a couple of loops for practice

--- lists.py
+++ lists.py
@@ -6,4 +6,11 @@ numbers[3]
 #output is '3'
 #...ok...so....that's that. Zzzzzzzz. I'm sure it will get better I'm just in syntax hell right now. 
 #Congrats if you're reading this. 
+#Ok, lets do a loop, it's much better and fun anyway. 
+for number in numbers:
+    print(number)
+#Let's print out any numbers in multiplied times themselves
+
+for number in numbers:
+    print(number**2)
 

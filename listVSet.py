9f300e1b03b4b52282b18111cafbf45ee77144d3
Joe Hill <joe@jkh3.com>
2018-03-31 22:38:10 -0400

    Explored just a bit with sets in Python and how to compare intersections between sets

---
+++ listVSet.py
@@ -0,0 +1,7 @@
+#looking at sets vs lists
+numbers = set()
+numbers.add(3)
+lottery_values = {3,5,17,6}
+user_values = {3,5,11,2}
+lottery_values.intersection(user_values) #this will give you common values between the two sets
+#super helpful in Joins in SQL I'm Sure
--- split.py
+++ split.py
@@ -1,3 +1,10 @@
 #Working on the String split() method
 numbers = input("Enter your numbers, separated by commas: ")
 numbers.split(",")
+numbers_as_int = []
+for number in numbers:
+    numbers_as_int.append(int(number))
+
+#Another way to do this
+
+[int(number) for number in numbers] #this will convert each variable in that list into an int

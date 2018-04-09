116364e1c3ee503ddfc47c30116ffda74cc6dba2
Joe Hill <joe@jkh3.com>
2018-04-08 23:00:53 -0400

    Learning about Passing by Reference, and .append method

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
-sample_dictionary = {"name": "Jose", 
-                    "mark": 70}
+def create_student():
+    # Ask the user for the student's name
+    name = input("Enter Student's Name: ")
+    student_data = {
+        'name': name,
+        'marks': []
+    }
 
-print(sample_dictionary["name"])
+s = create_student()
+
+def add_mark(student, mark):
+    # Append a mark to the student dict
+    student['marks'].append(mark)
+
+add_mark(s, 5) # Passing by reference
+
+
+print(s)
